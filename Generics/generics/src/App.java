bbef328df649dc5bf0267abd864b9eadca7963e7
Gustavo Habermann <xinbinhanaomorreu@gmail.com>
2025-06-16 17:02:23 -0300

    Generics and wildcards

---
+++ Generics/generics/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+
+    }
+}

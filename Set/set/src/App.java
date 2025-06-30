2e9ba67eaf4fd280c6d9beb78368198e95f606e9
Gustavo Habermann <xinbinhanaomorreu@gmail.com>
2025-06-30 17:09:37 -0300

    Comparator with arrow function

--- Set/set/src/App.java
+++ Set/set/src/App.java
@@ -1,19 +1,20 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entites.Product;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("Tv", 900.0));
         set.add(new Product("Notebook", 1200.0));
         set.add(new Product("Tablet", 400.0));
 
-        Product prod = new Product("Notebook", 1200.0);
-
-        System.out.println(set.contains(prod));
+        for (Product p : set){
+            System.out.println(p);
+        }
     }
 }

9e7c29dbe8b8b5c08e657ce4b09c79bb3a255856
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2026-08-03 14:32:29 -0300

    Fix orderitem repository/ orderResource

--- Section_22/curse/src/main/java/com/testespring/curse/repositories/OrderItemRepository.java
+++ Section_22/curse/src/main/java/com/testespring/curse/repositories/OrderItemRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.testespring.curse.entites.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
     
 }

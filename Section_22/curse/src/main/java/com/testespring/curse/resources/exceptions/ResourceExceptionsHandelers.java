05e57c02e21878e1c253607bf5aa2223b0dd5c4b
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2026-08-05 14:12:19 -0300

    Exception handling delete

--- Section_22/curse/src/main/java/com/testespring/curse/resources/exceptions/ResourceExceptionsHandelers.java
+++ Section_22/curse/src/main/java/com/testespring/curse/resources/exceptions/ResourceExceptionsHandelers.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.testespring.curse.services.exceptions.DataBaseExceptions;
 import com.testespring.curse.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,4 +24,13 @@ public class ResourceExceptionsHandelers {
         return ResponseEntity.status(status).body(err);
     }
     
+    @ExceptionHandler(DataBaseExceptions.class)
+    public ResponseEntity<StandardError> database(DataBaseExceptions e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
--- Section_22/curse/src/main/java/com/testespring/curse/services/UserSercive.java
+++ Section_22/curse/src/main/java/com/testespring/curse/services/UserSercive.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.testespring.curse.entites.User;
 import com.testespring.curse.repositories.UserRepository;
+import com.testespring.curse.services.exceptions.DataBaseExceptions;
 import com.testespring.curse.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserSercive {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataBaseExceptions(e.getMessage());
+        }
+        
     }
 
     public User update(Long id, User obj){
